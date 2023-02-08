158b3afd40048864d08ae8d99dced4ddf0d5ecfc
SinhueSiordia <carpediem.sinhue@gmail.com>
2023-02-08 11:07:03 -0700

    acciones de navegacion con navbar

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse('<h1>Bienvenido a la pagina de inicio<h1>')
+    return render(request, 'pages/inicio.html')
 
 def consulta(request):
     return render(request, 'pages/consulta.html')
