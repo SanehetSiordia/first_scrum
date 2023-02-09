aaf8e0a2dc1768fd780da782e111fb323f1c5a2f
SinhueSiordia <carpediem.sinhue@gmail.com>
2023-02-08 20:18:00 -0700

    set debug

--- scrudCoppel/settings.py
+++ scrudCoppel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0r%b-l#r#o8@vm&c&zuw)-bcc81p)!xww0h_tb@j#bn&1$+%ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
